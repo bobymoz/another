054c27cf94b40ddfabe9a38ec11301334aefd77c
bobymoz <itanhagira@gmail.com>
2026-03-31 18:36:48 +0200

    Refactor main function and update app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,20 @@
-import 'package:another_iptv_player/controllers/playlist_controller.dart';
-import 'package:another_iptv_player/screens/app_initializer_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:another_iptv_player/services/service_locator.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
-import 'controllers/locale_provider.dart';
-import 'controllers/theme_provider.dart';
-import 'l10n/app_localizations.dart';
-import 'l10n/supported_languages.dart';
-import 'utils/app_themes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/language_selection_screen.dart';
+// Importe aqui a sua tela principal que já veio no fork (ex: lib/screens/home_screen.dart)
 
-Future<void> main() async {
-  await setupServiceLocator();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => LocaleProvider()),
-        ChangeNotifierProvider(create: (_) => PlaylistController()),
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  final bool isFirstRun = prefs.getBool('first_run') ?? true;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final localeProvider = Provider.of<LocaleProvider>(context);
-    final themeProvider = Provider.of<ThemeProvider>(context);
-
-    return MaterialApp(
-      locale: localeProvider.locale,
-      supportedLocales:
-      supportedLanguages.map((lang) => Locale(lang['code'])).toList(),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      title: 'Another IPTV Player',
-      theme: AppThemes.lightTheme,
-      darkTheme: AppThemes.darkTheme,
-      themeMode: themeProvider.themeMode,
-      home: AppInitializerScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  runApp(MaterialApp(
+    title: 'PlayTVNow',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(),
+    home: isFirstRun ? const LanguageSelectionScreen() : const MinhaTelaPrincipal(), // Troque pelo nome da classe da sua Home
+    routes: {
+      '/home': (context) => const MinhaTelaPrincipal(), // Troque aqui também
+    },
+  ));
 }
